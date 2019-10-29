dbf736e6a6b8a3b6008eb571d05f76c16df3a52c
vgiabao <48625302+vgiabao@users.noreply.github.com>
2019-10-29 19:23:42 +0700

    fixed error when people try to modify data

---
+++ precache-manifest.8c3bcb888e6e7b7cc6dba762562a0927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d43af8aab9c0f5d51e12790931f604a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c13a72e44f3a81beee2d",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "425dfa43b923eb90aee9",
+    "url": "/static/css/main.963435ea.chunk.css"
+  },
+  {
+    "revision": "c13a72e44f3a81beee2d",
+    "url": "/static/js/2.fd4be3aa.chunk.js"
+  },
+  {
+    "revision": "425dfa43b923eb90aee9",
+    "url": "/static/js/main.65d86bd4.chunk.js"
+  },
+  {
+    "revision": "1894cba9bc11a70d2d89",
+    "url": "/static/js/runtime-main.2dc72dfa.js"
+  }
+]);
